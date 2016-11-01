c3334796b6a8da372ea2fb878e5e83457d52cd8e
Erik Larsson <erik.mattias.larsson@gmail.com>
2016-11-01 20:08:13 +0100

    Testing grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.initConfig({
       babel: {
@@ -13,15 +14,28 @@ module.exports = function(grunt) {
               files: [
                   {
                       expand: true,
-                      cwd: '/',
-                      src: ['*.js'],
+                      cwd: '.',
+                      src: ['app/*.js', 'lib/utils.js', 'lib/userInput.js'],
                       dest: 'dist/'
                   }
               ]
           }
       },
-      clean: ['dist']
+      clean: ['dist'],
+      copy: {
+        main: {
+          files: [
+            {
+              expand: true, src: [
+                'index.html',
+                'favicon.png',
+                'lib/require.js',
+                'lib/underscore.js',
+              ], dest: 'dist/', filter: 'isFile'}
+          ]
+        }
+      }
   });
 
-  grunt.registerTask('default', ['clean', 'babel']);
+  grunt.registerTask('default', ['clean', 'copy', 'babel']);
 };
