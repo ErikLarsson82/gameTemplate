9b862f7b5cff8f7dd7e3aaee41293d8cf0daee5d
Erik Larsson <erik.mattias.larsson@gmail.com>
2016-11-09 08:00:22 +0100

    Try to clean

--- app/game.js
+++ app/game.js
@@ -9,21 +9,53 @@ define('app/game', [
 ) {    
     
     let gameObjects = [];
+    var game = {}
+
 
     class GameObject {
         constructor(config) {
+            this.game = config.game;
             this.hitbox = config.hitbox;
             this.color = config.color || "#444444";
         }
-        tick(delta) {
+        tick() {
             //Will take approx 2783 to cross screen once
-            this.hitbox.x = (this.hitbox.x > canvas.width) ? 0 : this.hitbox.x + (1 * delta);
+            this.hitbox.x = (this.hitbox.x > canvas.width) ? 0 : this.hitbox.x + 1;
         }
         draw() {
             context.fillStyle = this.color;
             context.fillRect(this.hitbox.x, this.hitbox.y, this.hitbox.width, this.hitbox.height);
         }
     }
+
+    class Timer {
+        constructor() {
+            this.start = new Date();
+            this.amount = 0;
+        }
+        tick() {
+            this.amount = new Date().getTime() - this.start.getTime();
+        }
+        draw() {
+            context.fillStyle = "black";
+            context.fillText(Math.floor(this.amount/1000),100,100);
+        }
+    }
+
+    class DeltaTimer {
+        constructor() {
+            this.amount = 0;
+        }
+        tick(delta) {
+            this.amount += delta;
+        }
+        draw() {
+            context.fillStyle = "black";
+            context.fillText(Math.floor(this.amount/1000),100,150);
+        }
+    }
+
+    const delta = 1.0/144;
 
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
@@ -40,10 +72,14 @@ define('app/game', [
                     height: 10
                 }
             }));
+
+            gameObjects.push(new Timer());
+
+            gameObjects.push(new DeltaTimer());
         },
-        tick: function(delta) {
+        tick: function() {
             _.each(gameObjects, function(gameObject) {
-                gameObject.tick(delta);
+                gameObject.tick();
             });
 
             context.fillStyle = "white";
